26c57fce010ac06bf760751a892e66dcaf84c7df
Jack Estes <jack.estes@fbinv.com>
2026-05-06 14:44:07 -0600

    Update configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  build: { format: 'file' }
+  build: { format: 'file' },
+  site: "https://jackestes06.github.io",
+  base: "/WebDev/",
 });
