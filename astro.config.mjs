09edacc0bdf2b27f6af46196d1e94712a9ce649c
JackEstes06 <jackestes3210@gmail.com>
2026-06-14 17:40:02 -0600

    Stop tracking Firebase config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  integrations: [tailwind()],
-  build: { format: 'file' },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  build: { format: "file" },
   site: "https://jackestes06.github.io",
   base: "/WebDev/",
 });
